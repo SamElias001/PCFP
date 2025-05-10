440a15069226f5711f3697ce303881e1ad578feb
Joao Pedro Schiavoni <joaopschiavoni@gmail.com>
2025-05-10 15:51:40 -0300

    jsontest

--- firstTest.py
+++ firstTest.py
@@ -1,2 +1,12 @@
-print()
-
+import json
+description = 'Sorvete'
+price = 5.50
+date = '10/05/2025'
+data = {
+    'Descrição': description,
+    'Preço': price,
+    'Data': date,
+}
+# Convert the dictionary to a JSON string
+with open ('data.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
